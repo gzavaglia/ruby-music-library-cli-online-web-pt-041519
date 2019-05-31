239c9611af6d27974a26d7e7ee4b5ce3d88f4c2e
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-05-31 01:33:29 +0000

    Automatically backed up by Learn

--- lib/models/musiclib_controller.rb
+++ lib/models/musiclib_controller.rb
@@ -84,10 +84,10 @@ class MusicLibraryController
   end
   
   def play_song
-    list_songs
+    #list_songs
     puts "Which song number would you like to play?"
     #list_songs
-    user_song = gets.chomp.to_i
+    user_song = gets.strip.to_i
     if user_song > 0 && user_song <= Song.all.length
       sorted_array = Song.all.sort{|a,b| a.name <=> b.name}
       selection = sorted_array[user_song-1]
